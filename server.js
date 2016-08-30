de6f2c524e17281498736b34258265ea8bec24db
Caleb Aston <caleb.k.aston@gmail.com>
2016-08-29 20:49:41 -0600

    "Added in a functioning express server, added a package.json for my dependencies, and partailly completed the Press Release"

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-server.js
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+
+var router = express.Router();
+
+// http://localhost:8080/api
+
+router.get('/*', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Setting sail for ' + port);
